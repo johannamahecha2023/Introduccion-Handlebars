97c71daef8ef3290d823b6cfef48f834330c5451
johannamahecha2023 <johannamahechacp@gmail.com>
2024-12-11 21:45:07 -0500

    helper interno each

--- Handlebars/IntroduccionHB/routes/users.js
+++ Handlebars/IntroduccionHB/routes/users.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('enrutamiento para usuarios');
+  const usuarios=[
+    {nombre:'chulita',edad:3},
+    {nombre:'angel',edad:5},
+    {nombre:'nora',edad:4}  
+  ]
+  res.render('usuarios',{usuarios})
 });
 
 module.exports = router;
