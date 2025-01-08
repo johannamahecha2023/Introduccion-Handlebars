917b2f382a63c7ce07857060fbf66764a9cf0780
johannamahecha2023 <johannamahechacp@gmail.com>
2025-01-08 18:35:08 -0500

    registro profesores

--- Motores de plantillas/API-REST/routes/index.js
+++ Motores de plantillas/API-REST/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const { conexion }= require('../database/conexion')
 //importamos el modulo de mysql para poder concectarnos a la base de datos
 
-router.get('/', function(req, res, next) {
+router.get('/estudiantes', function(req, res, next) {
   conexion.query('select * from estudiantes;', (error,resultado)=>{
     if(error){
       res.status(500).send('ocurrio un error en la consulta')
@@ -12,5 +12,20 @@ router.get('/', function(req, res, next) {
     }
   })
 });
+router.get('/registro-profesores',(req,res) =>{
+  res.sendFile('registro-profesores.html', {root: 'public'})
+})
+router.get('/profesores', function(req, res, next) {
+  //consulta de profesores en la BD en sql
+  conexion.query('select * from profesores;', (error,resultado)=>{
+    if(error){
+      res.status(500).send('ocurrio un error en la consulta')
+    }else{
+      res.status(200).render('index',{ resultado, title: 'profesores'})
+    }
+  })
+});
+
+
 
 module.exports = router;
