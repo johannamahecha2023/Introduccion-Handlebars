7422202f4f7f51e1c2fea92fbf87c3f347baab4a
johannamahecha2023 <johannamahechacp@gmail.com>
2025-01-08 22:36:20 -0500

    enrutamiento de registro docente y visualizacion en la plantilla

--- Motores de plantillas/API-REST/routes/index.js
+++ Motores de plantillas/API-REST/routes/index.js
@@ -15,6 +15,22 @@ router.get('/estudiantes', function(req, res, next) {
 router.get('/registro-profesores',(req,res) =>{
   res.sendFile('registro-profesores.html', {root: 'public'})
 })
+router.post("/agregar-profesor", (req,res)=>{
+  const nombre= req.body.name
+  const apellido=req.body.lastname
+  const fecha_nacimiento=req.body.date
+  const genero=req.body.genero
+  const direccion=req.body.direccion
+  conexion.query(`INSERT INTO profesores (nombre, apellido, fecha_nacimiento, genero, direccion) VALUES ('${nombre}','${apellido}','${fecha_nacimiento}','${genero}','${direccion}')`,(error,resultado)=>{
+    if(error){
+      console.log(`ocurrio un error en la ejecucion ${error}`)
+      res.status(500).send('error en la consulta')
+    }
+    else{
+      res.status(200).redirect('/profesores')
+    }
+  })
+})
 router.get('/profesores', function(req, res, next) {
   //consulta de profesores en la BD en sql
   conexion.query('select * from profesores;', (error,resultado)=>{
